c511d0023f2dbd1c8775e3a13a3512cc047a2505
Emily Fernandez <emilyfern121@gmail.com>
2021-04-10 22:57:59 -0400

    adding function to store input in the local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,9 +2,33 @@
 var today = moment();
 $("#date").text(today.format("dddd MMMM Do, YYYY"));
 
+var textInput = document.querySelector("#event");
+var saveButton = document.querySelector("#save-button");
+
+renderLastInput()
+
+function renderLastInput() {
+    var event = localStorage.getItem("event");
+    if (!event) {
+        return;
+      }
+    userEventSpan.textContent = event;
+  }
 
 
-
-
+  saveButton.addEventListener("click", function(event) {
+    event.preventDefault();
+  
+    var event = document.querySelector("#event").value;
+  
+    if (email === "") {
+      displayMessage("error", "task cannot be blank");
+    } else {
+      displayMessage("success", "saved successfully");
+  
+      localStorage.setItem("event", event);
+      renderLastRegistered();
+    }
+  });
 
 
